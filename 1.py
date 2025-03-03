30a961f1f4a5bb550b66ef73d9dcdaecaddf4b98
acturus <82964583+acturus1@users.noreply.github.com>
2025-03-03 16:13:47 +0300

    Исправление 6 задачи

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QComboBox, QLineEdit, QHBoxLayout, QTextEdit, QCheckBox
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit, QHBoxLayout
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 import requests
@@ -8,14 +8,9 @@ class MapParams:
     def __init__(self):
         self.lat = 61.665279
         self.lon = 50.813492
+        self.zoom = 16
         self.spn = 0.0005
-        self.theme = 'light'
-        self.api = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
-        self.api2 = '8013b162-6b42-4997-9691-77b7074026e0'
-        self.marker_lat = None
-        self.marker_lon = None
-        self.address = ""
-        self.add_postcode = False
+
 
     def ll(self):
         return str(self.lon) + "," + str(self.lat)
@@ -28,7 +23,7 @@ class Window(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(100, 100, 600, 550)
+        self.setGeometry(100, 100, 600, 450)
         self.setWindowTitle('Карта')
 
         layout = QVBoxLayout()
@@ -40,20 +35,13 @@ class Window(QWidget):
         buttons_layout = QVBoxLayout()
         layout.addLayout(buttons_layout)
 
-        zoom_in_button = QPushButton('Уменьшить масштаб')
+        zoom_in_button = QPushButton('Увеличить масштаб')
         zoom_in_button.clicked.connect(self.zoom_in)
         buttons_layout.addWidget(zoom_in_button)
 
-        zoom_out_button = QPushButton('Увеличить масштаб')
+        zoom_out_button = QPushButton('Уменьшить масштаб')
         zoom_out_button.clicked.connect(self.zoom_out)
         buttons_layout.addWidget(zoom_out_button)
-
-        themes = QComboBox()
-        themes.addItem("Светлая")
-        themes.addItem("Тёмная")
-
-        themes.currentTextChanged.connect(self.change_theme)
-        buttons_layout.addWidget(themes)
 
         search_layout = QHBoxLayout()
         layout.addLayout(search_layout)
@@ -66,25 +54,10 @@ class Window(QWidget):
         search_button.clicked.connect(self.search_object)
         search_layout.addWidget(search_button)
 
-        reset_button = QPushButton('Сброс поискового результата')
-        reset_button.clicked.connect(self.reset_search_result)
-        search_layout.addWidget(reset_button)
-
-        postcode_checkbox = QCheckBox('Добавлять почтовый индекс')
-        postcode_checkbox.stateChanged.connect(self.toggle_postcode)
-        search_layout.addWidget(postcode_checkbox)
-
-        self.address_label = QTextEdit()
-        self.address_label.setReadOnly(True)
-        layout.addWidget(self.address_label)
-
         self.load_map()
 
     def load_map(self):
-        if self.mp.marker_lat and self.mp.marker_lon:
-            map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.mp.ll()}&theme={self.mp.theme}&spn={self.mp.spn},{self.mp.spn}&l=map&pt={self.mp.marker_lon},{self.mp.marker_lat},pm2al&apikey={self.mp.api}"
-        else:
-            map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.mp.ll()}&theme={self.mp.theme}&spn={self.mp.spn},{self.mp.spn}&l=map&apikey={self.mp.api}"
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.mp.ll()}&z={self.mp.zoom}&l=map&pt={self.mp.ll()},pm2al"
         response = requests.get(map_request)
         if response.status_code == 200:
             with open("map.png", "wb") as file:
@@ -94,15 +67,35 @@ class Window(QWidget):
             self.map_label.setPixmap(pixmap.scaled(600, 400, Qt.KeepAspectRatio))
 
     def zoom_in(self):
-        if self.mp.spn < 19:
-            self.mp.spn += 0.0001
+        if self.mp.zoom < 19:
+            self.mp.zoom += 1
             self.load_map()
 
     def zoom_out(self):
-        if self.mp.spn > 0.0001:
-            self.mp.spn -= 0.0001
+        if self.mp.zoom > 1:
+            self.mp.zoom -= 1
             self.load_map()
 
+    def search_object(self):
+        query = self.search_input.text()
+        if query:
+            geocoder_api_server = "https://geocode-maps.yandex.ru/1.x/"
+            geocoder_params = {
+                "format": "json",
+                "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
+                "geocode": query
+            }
+            response = requests.get(geocoder_api_server, params=geocoder_params)
+            if response:
+                json_response = response.json()
+                try:
+                    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+                    self.mp.lat = float(toponym["Point"]["pos"].split()[1])
+                    self.mp.lon = float(toponym["Point"]["pos"].split()[0])
+                    self.load_map()
+                except IndexError:
+                    print("не найденно")
+        
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_S:
             self.move_up()
@@ -132,80 +125,6 @@ class Window(QWidget):
         spn = self.mp.spn
         self.mp.lon = min(180, self.mp.lon + spn)
         self.load_map()
-
-    def change_theme(self, text):
-        if text == "Светлая":
-            self.mp.theme = "light"
-        elif text == 'Тёмная':
-            self.mp.theme = "dark"
-        self.load_map()
-
-    def search_object(self):
-        query = self.search_input.text()
-        if query:
-            geocoder_api_server = "https://geocode-maps.yandex.ru/1.x/"
-            geocoder_params = {
-                "format": "json",
-                "apikey": self.mp.api2,
-                "geocode": query
-            }
-            response = requests.get(geocoder_api_server, params=geocoder_params)
-            if response:
-                json_response = response.json()
-                try:
-                    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-                    self.mp.lat = float(toponym["Point"]["pos"].split()[1])
-                    self.mp.lon = float(toponym["Point"]["pos"].split()[0])
-                    self.mp.marker_lat = self.mp.lat
-                    self.mp.marker_lon = self.mp.lon
-                    address = toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]["formatted"]
-                    if self.mp.add_postcode:
-                        self.mp.address = address
-                    else:
-                        parts = address.split(', ')
-                        if len(parts) > 2:
-                            self.mp.address = ', '.join(parts[:-1])
-                        else:
-                            self.mp.address = address
-                    self.address_label.setText(self.mp.address)
-                    self.load_map()
-                except IndexError:
-                    print("Объект не найден")
-
-    def reset_search_result(self):
-        self.mp.marker_lat = None
-        self.mp.marker_lon = None
-        self.mp.address = ""
-        self.address_label.setText("")
-        self.load_map()
-
-    def toggle_postcode(self, state):
-        self.mp.add_postcode = state == Qt.Checked
-        if self.mp.address:
-            geocoder_api_server = "https://geocode-maps.yandex.ru/1.x/"
-            geocoder_params = {
-                "format": "json",
-                "apikey": self.mp.api2,
-                "geocode": self.mp.address
-            }
-            response = requests.get(geocoder_api_server, params=geocoder_params)
-            print(response.url)
-            if response:
-                json_response = response.json()
-                try:
-                    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-                    address = toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]["formatted"]
-                    if self.mp.add_postcode:
-                        self.mp.address = address
-                    else:
-                        parts = address.split(', ')
-                        if len(parts) > 2:
-                            self.mp.address = ', '.join(parts[:-1])
-                        else:
-                            self.mp.address = address
-                    self.address_label.setText(self.mp.address)
-                except IndexError:
-                    print("Объект не найден")
 
 def main():
     app = QApplication(sys.argv)
